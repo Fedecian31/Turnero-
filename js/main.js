feefd95948e96a420e1bbb77f6080e278f6faf54
Federico Cian <fedecian31@gmail.com>
2025-11-05 16:50:29 -0300

    arreglo

--- js/main.js
+++ js/main.js
@@ -1,40 +1,45 @@
 let doctores = [];
 let cardsDoctores = document.getElementById("cardsDoctores");
+const URL = "./db/data.json";
 
-async function cargarDoctores() {
-    try {
-        const respuesta = await fetch("./data/odontologos.json");
-        if (!respuesta.ok) throw new Error("Error al cargar los datos");
-        doctores = await respuesta.json();
-        renderOdontologos(doctores);
-    } catch (error) {
-        console.error("Hubo un problema al traer los doctores:", error);
-    } finally {
-        console.log("Carga de odontólogos finalizada.");
-    }
+function cargarDoctores() {
+    fetch(URL)
+        .then((respuesta) => respuesta.json())
+        .then((data) => {
+            doctores = data;
+            renderOdontologos(doctores);
+        })
+        .catch((error) => {
+            console.error("Hubo un problema al traer los doctores:", error);
+        })
+        .finally(() => {
+            console.log("Carga de odontólogos finalizada.");
+        });
 }
 
 function renderOdontologos(doctores) {
+    cardsDoctores.innerHTML = "";
+
     doctores.forEach((doctor) => {
         let card = document.createElement("div");
         card.innerHTML = `
-    <h3>Doctor: ${doctor.nombre}</h3>
-    <p>Tratamientos: ${doctor.tratamientos}</p>
-    <a id="${doctor.id}" href="../pages/formulario.html" class="btnReservar">Reservar Turno</a>`;
-
+        <h3>Doctor: ${doctor.nombre}</h3>
+        <p>Tratamientos: ${doctor.tratamientos.join(", ")}</p>
+        <a id="${doctor.id}" href="./pages/formulario.html" class="btnReservar">Reservar Turno</a>
+    `;
         cardsDoctores.appendChild(card);
     });
+
     reservarButton();
 }
 
 function reservarButton() {
-    let addButton = document.querySelectorAll(".btnReservar");
-    addButton.forEach((link) => {
+    const botones = document.querySelectorAll(".btnReservar");
+
+    botones.forEach((link) => {
         link.onclick = () => {
             const odontologoId = Number(link.id);
-            const odontologoElegido = doctores.find(
-                (doctor) => doctor.id == odontologoId
-            );
+            const odontologoElegido = doctores.find((d) => d.id === odontologoId);
 
             localStorage.setItem(
                 "odontologoSeleccionado",
@@ -44,5 +49,4 @@ function reservarButton() {
     });
 }
 
-cargarDoctores()
-renderOdontologos(doctores);
+cargarDoctores();
